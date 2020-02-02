400a8584dd80f4708f19af98755e3c8e77965277
JonahTuckman <Jtuckman9@gmail.com>
2020-02-02 15:07:51 -0500

    Formatting

--- Teaching_String_Concat.py
+++ Teaching_String_Concat.py
@@ -8,17 +8,17 @@ number_4 = 4
 strang = "Word"
 strung = "Werd"
 
-#####################
-# Concat using +
-#####################
+##########################################
+# Concat Using +
+##########################################
 
 print("This sentance has the numbers " + str(number_1) + str(number_2) + str(number_3) + str(number_4) + " as well as some words like " + strang + " and " + strung)
 
 # This is not best as you can see the need to type cast 4 different ints as well as continue to use the + sign over and over
 
-#####################
-# Using % signs
-#####################
+##########################################
+# Concat Using %
+##########################################
 
 print("This sentance has the numbers %d%d%d%d as well as some words like %s and %s" % (number_1, number_2, number_3, number_4, strang, strung))
 
