5350921710aed32494a4f19ded7f38441a4c282a
JonahTuckman <Jtuckman9@gmail.com>
2020-02-24 11:07:30 -0500

    done

---
+++ BioInformatics/Conditional_Assignment/length_range.py
@@ -0,0 +1,13 @@
+import pandas as pd
+# Using pandas to read the data because it is great
+
+# reading the file
+data = pd.read_csv('data.csv', header = None)
+ 
+# iterating over the different rows so that I can check the length of the gene names
+for index, row in data.iterrows():
+    # conditional check
+    # The data is unlabelled so I am assuming that this is what is intended
+    if len(row[1]) >= 90 and len(row[1]) <= 110:
+        print(row[1])
+        
