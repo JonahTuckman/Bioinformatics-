173625c2a739a70354b892ecfe684b933637d7bb
jtuckman9@gmail.com <Jtuckman9@gmail.com>
2020-04-22 00:07:51 -0400

    changes

--- BioInformatics/Blast_Parser/blast_parser.py
+++ BioInformatics/Blast_Parser/blast_parser.py
@@ -81,28 +81,26 @@ for line in blast_file:
    
     elif state == S3:
     # Here is the issue, with the regex in state 3
-        match_nohits = re.search(r'[*]+\s+No\s+hits\s+found\s+[*]+', line)
         match = re.search(r'Query:\s+(([\w\.])+)\s+([\w\.])', line)
         if match:
             query = match.group(1)
             m_lett = match.group(2)
             state = S4
                 
+        match_nohits = re.search(r'[*]+\s+No\s+hits\s+found\s+[*]+', line)
         if match_nohits:
-            state = S1
-        else:
             state = S1
            
     # We need to print in state 4 solely
     elif state == S4:
         match = re.search(r'Sbjct:\s+(([\w\.])+)\s+([\w\.])', line)
         if match == 3:
-            first_EST = match.group(1)
-            second_EST = match.group(2)
-            if second_EST == "M":
+            first = match.group(1)
+            second = match.group(2)
+            if second == "M":
             # Not printing anything so I would expect that the error is somewhere in this area
                 print("%s %d %s" % (current_EST, length, query))
-        state = S1
+                state = S1
     # add elif's for the missing states, one at a time
     # advice: add the missing states in order - S2, S3, S4
 
